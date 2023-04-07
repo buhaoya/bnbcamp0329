37ee9ba96ac080360469a2989f95b3b4ced3db50
z <z@163.com>
2023-04-07 21:48:26 +0800

    更新合约代码并升级

--- contracts/StandardProxy/StandardImpl.sol
+++ contracts/StandardProxy/StandardImpl.sol
@@ -9,6 +9,11 @@ contract StandardImpl {
     bool public initialized;
 
     uint256 public value;
+    bytes32 public hashValue;
+    uint256[2] public numbers = [1, 2];
+    uint8 public shortNumberA;
+    uint8 public shortNumberB;
+    uint128 public shortNumberC;
 
     modifier initializer() {
         require(!initialized, "Only initialize once");
@@ -22,5 +27,25 @@ contract StandardImpl {
 
     function setValue(uint256 _newValue) public {
         value = _newValue + 10;
+    }
+
+    function setHashvalue(bytes32 _newValue) public {
+        hashValue = _newValue;
+    }
+
+    function setNumbers(uint256[2] memory _newValue) public {
+        numbers = _newValue;
+    }
+
+    function setShortNumberA(uint8 _newValue) public {
+        shortNumberA = _newValue;
+    }
+
+    function setShortNumberB(uint8 _newValue) public {
+        shortNumberB = _newValue;
+    }
+
+    function setShortNumberC(uint128 _newValue) public {
+        shortNumberC = _newValue;
     }
 }
--- tasks/index.ts
+++ tasks/index.ts
@@ -42,3 +42,133 @@ task("setValue")
     const currentValue = await myContract.value();
     console.log("currentValue: ", currentValue.toString());
   });
+
+task("getHashvalue").setAction(async (_, hre) => {
+  const { network } = hre;
+  const [dev] = await hre.ethers.getSigners();
+  const addressList = readAddressList();
+
+  const myContract = new StandardImpl__factory(dev).attach(
+    addressList[network.name].MyContract
+  );
+  const value = await myContract.hashValue();
+  console.log("hashValue: ", hre.ethers.utils.parseBytes32String(value.toString()));
+});
+
+task("setHashvalue")
+  .addParam("value", "The hashValue to set", undefined, types.string)
+  .setAction(async (taskArgs, hre) => {
+    const { network } = hre;
+    const [dev] = await hre.ethers.getSigners();
+    const addressList = readAddressList();
+
+    const myContract = new StandardImpl__factory(dev).attach(
+      addressList[network.name].MyContract
+    );
+    const newValue = hre.ethers.utils.formatBytes32String(taskArgs.value);
+    const tx = await myContract.setHashvalue(newValue);
+    console.log("tx: ", await tx.wait());
+
+    const currentValue = await myContract.hashValue();
+    console.log("currentValue: ", currentValue.toString());
+  });
+
+task("getNumbers").setAction(async (_, hre) => {
+  const { network } = hre;
+  const [dev] = await hre.ethers.getSigners();
+  const addressList = readAddressList();
+
+  const myContract = new StandardImpl__factory(dev).attach(
+    addressList[network.name].MyContract
+  );
+  // const value = await myContract.numbers({});
+  const value = await myContract.numbers(1);
+  console.log("numbers Value: ", value);
+});
+
+task("getShortNumberA").setAction(async (_, hre) => {
+  const { network } = hre;
+  const [dev] = await hre.ethers.getSigners();
+  const addressList = readAddressList();
+
+  const myContract = new StandardImpl__factory(dev).attach(
+    addressList[network.name].MyContract
+  );
+  const value = await myContract.shortNumberA();
+  console.log("shortNumberA Value: ", value.toString());
+});
+
+task("setShortNumberA")
+  .addParam("value", "The value to set", undefined, types.int)
+  .setAction(async (taskArgs, hre) => {
+    const { network } = hre;
+    const [dev] = await hre.ethers.getSigners();
+    const addressList = readAddressList();
+
+    const myContract = new StandardImpl__factory(dev).attach(
+      addressList[network.name].MyContract
+    );
+    const tx = await myContract.setShortNumberA(taskArgs.value);
+    console.log("tx: ", await tx.wait());
+
+    const currentValue = await myContract.shortNumberA();
+    console.log("currentValue: ", currentValue.toString());
+  });
+
+task("getShortNumberB").setAction(async (_, hre) => {
+  const { network } = hre;
+  const [dev] = await hre.ethers.getSigners();
+  const addressList = readAddressList();
+
+  const myContract = new StandardImpl__factory(dev).attach(
+    addressList[network.name].MyContract
+  );
+  const value = await myContract.shortNumberB();
+  console.log("shortNumberB Value: ", value.toString());
+});
+
+task("setShortNumberB")
+  .addParam("value", "The value to set", undefined, types.int)
+  .setAction(async (taskArgs, hre) => {
+    const { network } = hre;
+    const [dev] = await hre.ethers.getSigners();
+    const addressList = readAddressList();
+
+    const myContract = new StandardImpl__factory(dev).attach(
+      addressList[network.name].MyContract
+    );
+    const tx = await myContract.setShortNumberB(taskArgs.value);
+    console.log("tx: ", await tx.wait());
+
+    const currentValue = await myContract.shortNumberB();
+    console.log("currentValue: ", currentValue.toString());
+  });
+
+task("getShortNumberC").setAction(async (_, hre) => {
+  const { network } = hre;
+  const [dev] = await hre.ethers.getSigners();
+  const addressList = readAddressList();
+
+  const myContract = new StandardImpl__factory(dev).attach(
+    addressList[network.name].MyContract
+  );
+  const value = await myContract.shortNumberC();
+  console.log("shortNumberC Value: ", value.toString());
+});
+
+task("setShortNumberC")
+  .addParam("value", "The value to set", undefined, types.int)
+  .setAction(async (taskArgs, hre) => {
+    const { network } = hre;
+    const [dev] = await hre.ethers.getSigners();
+    const addressList = readAddressList();
+
+    const myContract = new StandardImpl__factory(dev).attach(
+      addressList[network.name].MyContract
+    );
+    const tx = await myContract.setShortNumberC(taskArgs.value);
+    console.log("tx: ", await tx.wait());
+
+    const currentValue = await myContract.shortNumberC();
+    console.log("currentValue: ", currentValue.toString());
+  });
